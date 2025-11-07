ecf680f35bafc265db8355f2739882937717a32d
ChaJunWoo <wnsdnxla123@gmail.com>
2025-11-08 00:25:33 +0900

    feat: 메타데이터 저장을 위해 repository 추가

---
+++ src/main/java/com/cha/markit/repository/WatermarkRepository.java
@@ -0,0 +1,9 @@
+package com.cha.markit.repository;
+
+import com.cha.markit.domain.Watermark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WatermarkRepository extends JpaRepository<Watermark, String> {
+}
