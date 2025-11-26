b8af8a9e1d0051085346fd18112dfc64608a8f9a
ChaJunWoo <wnsdnxla123@gmail.com>
2025-11-26 21:03:15 +0900

    chore: AWS S3 설정 업데이트

--- src/main/java/com/cha/markit/aws/S3Service.java
+++ src/main/java/com/cha/markit/aws/S3Service.java
@@ -1,4 +1,4 @@
-package com.cha.markit.s3;
+package com.cha.markit.aws;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.async.AsyncRequestBody;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -23,19 +24,19 @@ public class S3Service {
     private final S3AsyncClient s3AsyncClient;
     private final S3Presigner s3Presigner;
 
-    @Value("${aws.s3.bucket}")
+    @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
-    public CompletableFuture<String> uploadBytesAsync(String key, byte[] zipData) {
-        log.info("S3 업로드 시작 - key: {}, size: {} bytes", key, zipData.length);
+    public CompletableFuture<String> uploadBytesAsync(String key, byte[] data, String contentType) {
+        log.info("S3 업로드 시작 - key: {}, size: {} bytes, contentType: {}", key, data.length, contentType);
 
         PutObjectRequest objectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
-                .contentType("application/zip")
+                .contentType(contentType)
                 .build();
 
-        return s3AsyncClient.putObject(objectRequest, AsyncRequestBody.fromBytes(zipData))
+        return s3AsyncClient.putObject(objectRequest, AsyncRequestBody.fromBytes(data))
                 .thenApply(response -> {
                     log.info("S3 업로드 완료 - key: {}, ETag: {}", key, response.eTag());
 
@@ -62,5 +63,24 @@ public class S3Service {
         PresignedGetObjectRequest presignedRequest = s3Presigner.presignGetObject(presignRequest);
 
         return presignedRequest.url().toString();
+    }
+
+    public CompletableFuture<Object> deleteAsync(String key) {
+        log.info("S3 삭제 시작 - key: {}", key);
+
+        DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        return s3AsyncClient.deleteObject(deleteRequest)
+                .thenApply(response -> {
+                    log.info("S3 삭제 완료 - key: {}", key);
+                    return null;
+                })
+                .exceptionally(e -> {
+                    log.error("S3 삭제 실패 - key: {}", key, e);
+                    throw new RuntimeException("S3 삭제 실패: " + e.getMessage(), e);
+                });
     }
 }
